36fe4bdc76ad9d6bddb5c91ac910a7af34457f91
Nature <874572491@qq.com>
2022-01-26 11:31:22 +0800

    创建路由

---
+++ src/views/Labels.vue
@@ -0,0 +1,13 @@
+<template>
+<div>labels.vue</div>
+</template>
+
+<script>
+export default {
+  name: 'Labels.vue'
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Statistics.vue
@@ -0,0 +1,13 @@
+<template>
+<div>statistics.vue</div>
+</template>
+
+<script>
+export default {
+  name: 'Statistics.vue'
+};
+</script>
+
+<style scoped>
+
+</style>
