421778bec54d3ebac169a17440ff4ed4cba8405b
Nature <874572491@qq.com>
2022-01-27 00:17:35 +0800

    引入svg目录代码上传

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,21 +1,35 @@
 <template>
-  <div class="nav" >
-    <router-link to="/money">记账</router-link>
+  <div class="nav">
+    <router-link to="/money">
+      <svg>
+        <use xlink:href="#label"/>
+      </svg>
+      记账
+    </router-link>
     |
     <router-link to="/labels">标签</router-link>
     |
     <router-link to="/statistics">统计</router-link>
-    |
   </div>
 </template>
 
 <script lang="ts">
+//下面这段代码是将一个目录中的所有svg引入
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {
+  importAll(require.context('../assets/icons', true, /\.svg$/));
+} catch (error) {
+  console.log(error);
+}
+
 export default {
   name: 'Nav'
 };
 </script>
 
 <style lang="scss" scoped>
- .nav{border: 1px solid red;}
+.nav {
+  border: 1px solid red;
+}
 
 </style>
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,6 +1,7 @@
 import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
+// @ts-ignore
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
